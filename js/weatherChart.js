4f6f196bcd4a19c5ad8a7c92eed7cdf1ecaaac76
apradhn <abhijit.pradhan6@gmail.com>
2015-07-21 18:37:46 -0400

    passed specs

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,52 +1,74 @@
 $(document).ready(function() {
   var API_KEY = '49dd8f14d7aa0798';
-  var URL = 'http://api.wunderground.com/api/49dd8f14d7aa0798/hourly/q/NY/New_York_City.json'; 
+  var url = 'http://api.wunderground.com/api/49dd8f14d7aa0798/hourly/q/NY/New_York_City.json'; 
 
-  $.getJSON(URL).done(function(data) {
-    console.log(data);
-    buildChart(data.hourly_forecast);
-  });
-
-  function buildChart(forecastData) {
-    var temps = [];
-    var labels = [];
-    var ctx = document.getElementById('NYCWeatherChart').getContext('2d');
-    var lineChart;
-    var label;
-    var hour;
-    var temp;
-    var data;
-
-    for (var i = 0; i < forecastData.length; i++) {
-      hour = forecastData[i];
-      temp = Number(hour.feelslike.english);
-      label = hour.FCTTIME.hour;
-      temps.push(temp);
-      labels.push(label);
-    };
-
-    console.log(data);
-    console.log(labels);
-
-    data = {
-      labels: labels,
-      datasets: [
-        {
-          fillColor: "rgba(220,220,220,0.2)",
-          strokeColor: "rgba(220,220,220,1)",
-          pointColor: "rgba(220,220,220,1)",
-          pointStrokeColor: "#fff",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(220,220,220,1)",
-          data: temps    
-        }
-      ]
-    }
-
-    lineChart = new Chart(ctx).Line(data);
-  }
-
-
+  makeAjaxRequest(url, buildChart);
 
 });
+
+function makeAjaxRequest(url, callback) {
+  $.ajax({
+    url: url,
+    dataType: 'jsonp',
+    success: callback
+  });
+}
+
+function buildChart(data) {
+  var temps = getFarenheits(data);
+  var hours = getHours(data);
+  var data = generateDataSet(hours, data);
+  var ctx = document.getElementsByTagName('canvas')[0].getContext('2d');
+  var lineChart = new Chart(ctx).Line(data);
+}
+
+function getFarenheits(data) {
+  data = data.hourly_forecast;
+  var temps = [];
+  var hour;
+  var temp;
+
+  for (var i = 0; i < data.length; i++) {
+    hour = data[i];
+    temp = hour.feelslike.english;
+    temps.push(temp);
+  };
+
+  return temps;
+}
+
+function getHours(data) {
+  data = data.hourly_forecast;
+  var hours = [];
+  var hour;
+
+  for (var i = 0; i < data.length; i++) {
+    hour = data[i].FCTTIME.hour;
+    hours.push(hour);
+  };
+
+  return hours;
+}
+
+function generateDataSet(labels, data) {
+
+  var dataset = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Hourly Weather for New York",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: data    
+      }
+    ]
+  }
+  console.log(dataset);
+  return dataset;
+
+}
 
